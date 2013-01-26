2a7aa08fed18fb8d34ad22f5b77bddd1db6e4bf4
helloxyz <helloxyz425@gmail.com>
2013-01-26 22:46:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportFans::Application.config.secret_token = 'a8ecf361b212eefaafc960d0b59ef9fcdced4101f725419a069de42f802e5eafb6c1ca75c0e09fb71ef161add16824107fa2e5bd635ebc9f199772c546341e1a'
